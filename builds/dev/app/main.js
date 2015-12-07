11a28ba1fa0d2720fe5fa584d71a4174862dcd63
KiraShubik <kira.shubik@gmail.com>
2015-12-07 17:45:02 +0300

    Beautify

--- builds/dev/app/main.js
+++ builds/dev/app/main.js
@@ -1,4 +1,5 @@
-;(function(){
+;
+(function() {
   'use strict';
 
   angular
@@ -11,34 +12,39 @@
     .run(MainRun)
     .config(MainConfig);
 
-    // @ngInject
-    function MainController ($rootScope) {
-      var s = this;
+  // @ngInject
+  function MainController($rootScope) {
+    var s = this;
 
-      s.hello_message = "Привет, мир!";
-      $rootScope.root = 'Root 1';
-    }
+    s.hello_message = "Привет, мир!";
+    $rootScope.root = 'Root 1';
+  }
 
-    // @ngInject
-    function MainRun($log, $rootScope){
-      $log.debug('Main Run');
+  // @ngInject
+  function MainRun($log, $rootScope) {
+    $log.debug('Main Run');
 
-      $rootScope.alerts = [];
-      $rootScope.addAlert = function(_msg, _type){
-        _type = _type || 'warning';
-        $rootScope.alerts.push({msg: _msg, type: _type});
-      };
+    $rootScope.alerts = [];
+    $rootScope.addAlert = function(_msg, _type) {
+      _type = _type || 'warning';
+      $rootScope.alerts.push({
+        msg: _msg,
+        type: _type
+      });
+    };
 
-      $rootScope.closeAlert = function(index) {
-        $rootScope.alerts.splice(index, 1);
-      };
-    }
+    $rootScope.closeAlert = function(index) {
+      $rootScope.alerts.splice(index, 1);
+    };
+  }
 
-    // @ngInject
-    function MainConfig ($routeProvider, $logProvider) {
-      console.log('Main Config');
-      $routeProvider.otherwise({ redirectTo: '/' });
-      $logProvider.debugEnabled(false);
-    }
+  // @ngInject
+  function MainConfig($routeProvider, $logProvider) {
+    console.log('Main Config');
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+    $logProvider.debugEnabled(false);
+  }
 
 })();
